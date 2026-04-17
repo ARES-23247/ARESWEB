3cd368c35aadf7248beee80b8bf569cd6eec5cc0
David G Huss <34256796+thehomelessguy@users.noreply.github.com>
2026-04-17 14:52:53 -0400

    Disable next/image optimization to prevent Cloudflare Edge Worker crashes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
